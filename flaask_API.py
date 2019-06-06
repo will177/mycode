67f934ec10f1e7634528230b923e17d2cae40899
Will Velez <fake@fake.example.com>
2019-06-06 10:40:48 -0400

    more API

--- flaask_API.py
+++ flaask_API.py
@@ -8,5 +8,12 @@ app = Flask(__name__)
 def endoftheday():
     return "Class is nearing the end for Wednesday"
 
+@app.route("/hello/<name>", defaults={'position': 'Administrative Assistant'})
+@app.route("/hello/<name>/<position>")
+def hellostudents(name, position):
+    return "Hello {1} {0}. I am pleased to meet you {0}".format(name, position)
+# return "Welcome to class ....."
+
+
 if __name__ == "__main__":
     app.run(port=5006)
